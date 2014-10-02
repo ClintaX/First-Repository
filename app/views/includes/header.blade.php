eb621b4e76587f125c95ca757b292e802924babd
ClintaX <clint_ariola@yahoo.com>
2014-10-02 14:02:12 +0800

    First Commit

---
+++ app/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+	<div class="navbar-inner">
+		<a id="logo" href="/">Single Malt</a>
+		<ul class="nav">
+			<li><a href="/MarketOnline/">Home</a></li>
+			<li><a href="/MarketOnline/about">About</a></li>
+			<li><a href="/MarketOnline/projects">Projects</a></li>
+			<li><a href="/MarketOnline/contact">Contact</a></li>
+		</ul>
+	</div>
+</div>
