e5a9ae31cc4c9f42cd4b6dd3debaea3047e4b881
ClintaX <clint_ariola@yahoo.com>
2014-10-02 17:41:07 +0800

    b

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -3,5 +3,8 @@
 <meta name="author" content="Clint D. Ariola">
 {{ HTML::style('assets/css/bootstrap.css') }}
 {{ HTML::style('assets/css/bootstrap-theme.min.css') }}
+{{ HTML::script('assets/js/jquery.js') }}
 {{ HTML::script('assets/js/bootstrap.min.js') }}
+{{ HTML::script('assets/js/_header.js') }}
+
 <title>Market Online</title>
--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -1,11 +1,21 @@
-<div class="navbar">
-	<div class="navbar-inner">
-		<a id="logo" href="/MarketOnline/">Single Malt</a>
-		<ul class="nav">
-			<li><a href="/MarketOnline/">Home</a></li>
-			<li><a href="/MarketOnline/about">About</a></li>
-			<li><a href="/MarketOnline/projects">Projects</a></li>
-			<li><a href="/MarketOnline/contact">Contact</a></li>
-		</ul>
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<ul class="nav nav-tabs">
+				<li class = 'c_menu' >
+					<a href="/MarketOnline/">Home</a>
+				</li>
+				<li class = 'c_menu' >
+					<a href="/MarketOnline/about">About</a>
+				</li>
+				<li class = 'c_menu' >
+					<a href="/MarketOnline/projects">Project</a>
+				</li>
+				<li class = 'c_menu' >
+					<a href="/MarketOnline/contact">Contact</a>
+				</li>
+			
+			</ul>
+		</div>
 	</div>
 </div>
